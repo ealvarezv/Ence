c76744b3e08e851054546fc0b5f0344b54a2caca
Álvarez Villacé <ealvarezv@minsait.com>
2020-08-20 18:56:08 +0200

    b

--- main.py
+++ main.py
@@ -24,8 +24,10 @@ outputFolder = "_output/"
 def main():
     print("\n#################### START ####################")
 
+    i = 0
     listFile = os.listdir(inputFolder)
     for file in listFile:
+        i += 1
         arrayRawPosition = []
         arrayRawAccuracy = []
         arraySmoothedPosition = []
@@ -33,6 +35,7 @@ def main():
 
         modifyJsonFormat(inputFolder, outputFolder, file)
 
+        drawFile = os.path.join(outputFolder, file).replace("log", "png")
         file = os.path.join(outputFolder, file)
         parserFile = json.load(open(file, "r"))
 
@@ -42,10 +45,10 @@ def main():
             arraySmoothedPosition.append(register["smoothedPosition"])
             arraySmoothedAccuracy.append(register["smoothedPositionAccuracy"])
 
-        printResult(arrayRawPosition, arrayRawAccuracy)
+        printResult(arrayRawPosition, arrayRawAccuracy, i)
+        plt.savefig(drawFile)
 
-        print(round(sum(arrayRawAccuracy)/len(arrayRawAccuracy), 6))
-        print(round(sum(arraySmoothedAccuracy)/len(arraySmoothedAccuracy), 6))
+    plt.show()
     print("\n#################### GAME OVER ####################\n\n")
 
 
@@ -76,13 +79,38 @@ def modifyJsonFormat(inputFolder, outputFolder, file):
 
 
 # Function to print the results in a graph
-def printResult(arrayRawPosition, arrayRawAccuracy):
-    print(arrayRawPosition)
-    print(arrayRawAccuracy)
-    plt.plot(arrayRawPosition, 'bx')
+def printResult(arrayRawPosition, arrayRawAccuracy, location):
+    i = 0
 
-    plt.savefig("hola.png")
-    plt.show()
+    plt.subplot(4, 4, location)
+    for position in arrayRawPosition:
+        plt.plot(position[0], position[1], createPoint(arrayRawAccuracy, i))
+        i += 1
+
+    rawAccuracy = round(sum(arrayRawAccuracy) / len(arrayRawAccuracy), 6)
+    smoothedAccuracy = round(sum(arrayRawAccuracy) / len(arrayRawAccuracy), 6)
+    plt.title("Raw Accuracy: " + str(rawAccuracy) + " / SmoothedAccuracy: "
+              + str(smoothedAccuracy))
+
+
+# Function to create the point with the correct colour
+# 0.00 - 0.50   'g'	green
+# 0.50 - 0.75   'y'	yellow
+# 0.75 - 1.00   'r'	red
+# 1.00 - 1.25   'm'	magenta
+# > 1.25        'k'	black
+def createPoint(array, position):
+    accuracy = array[position]
+    if accuracy <= 0.5:
+        return("g.")
+    elif accuracy <= 0.75:
+        return("y.")
+    elif accuracy <= 1:
+        return("r.")
+    elif accuracy <= 1.25:
+        return("m.")
+    else:
+        return("k.")
 
 
 # ################### EJECUCIÓN ####################
